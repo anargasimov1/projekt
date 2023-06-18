c1df44586f5cb5c14da1fa3b8f43d82e3d0ee0ab
anargasimov1 <anar.gasimov.89@gmail.com>
2023-06-18 22:42:28 +0400

    v1.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 "use srtict";
+// hamburger menu
+const btn = document.getElementById("btn"),
+    menu = document.getElementById("menu");
+
+let modal = false;
+
+btn.addEventListener("click", () => {
+    modal = !modal;
+    modal === true ? menu.style.display = "flex" : menu.style.display = "none";
+});
 
