1da1e6f908eb1ec482f141432c0a943100e4eac3
anargasimov1 <anar.gasimov.89@gmail.com>
2023-06-19 23:08:23 +0400

    v1.2

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,22 @@ btn.addEventListener("click", () => {
     modal === true ? menu.style.display = "flex" : menu.style.display = "none";
 });
 
+// swiper (main)
+
+const btn_left = document.getElementById("btn_left"),
+    btn_rigth = document.getElementById("btn_rigth"),
+    swiper = document.getElementById("swiper_slide");
+let index = 0;
+
+btn_rigth.addEventListener("click", () => {
+    index++;
+    if (index < swiper.children.length - 1)
+      {  swiper.style.transform = `translateX(${20 * -index}%)`};
+
+})
+btn_left.addEventListener("click", () => {
+    index--;
+    if (index > 0)
+      {  swiper.style.transform = `translateX(${20 * -index}%)`}
+
+})
